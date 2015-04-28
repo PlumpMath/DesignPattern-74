f601ceaad9fcc13cd5ab8240428e7bff1727f96c
wasir <wasir@wasir-PC>
2015-04-28 21:55:38 +0600

    first commited

--- src/duckplay/Duck.java
+++ src/duckplay/Duck.java
@@ -4,29 +4,33 @@
  */
 package duckplay;
 
-import com.interfacelist.QuackBehavior;
 import com.interfacelist.FlyBehavior;
+import com.interfacelist.QuackBehavior;
 
 /**
  *
  * @author wasir
  */
 public abstract class Duck {
+
     String wasir;
     FlyBehavior flyBehavior;
     QuackBehavior quackBehavior;
 
     public Duck() {
     }
+
     public abstract void display();
-    
-    public void performFly(){
+
+    public void performFly() {
         flyBehavior.fly();
     }
-    public void performQuack(){
+
+    public void performQuack() {
         quackBehavior.quack();
     }
-    public void swim(){
-       System.out.print("All ducks float. even decoys");
+
+    public void swim() {
+        System.out.print("All ducks float. even decoys");
     }
 }
